cc53c5f5bebef4aa897136c884b87596e496a2f6
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2023-05-30 20:03:40 +0200

    sorting method implemented

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
-console.log('hi');
+class Sorter {
+  constructor(public collection: number[]) {}
 
-console.log('hello');
+  sort(): void {
+    for (let i = this.collection.length; i >= 1; i--) {
+      for (let j = 0; j < i; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const key = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = key;
+        }
+      }
+    }
+  }
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
